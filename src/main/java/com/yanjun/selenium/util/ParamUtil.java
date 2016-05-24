b78ba11e754aa5b6c77c75e6b1c51457809fac99
YJ <yjciacia@gmail.com>
2016-05-24 14:05:15 +0800

    2016-05-24

--- src/main/java/com/yanjun/selenium/util/ParamUtil.java
+++ src/main/java/com/yanjun/selenium/util/ParamUtil.java
@@ -16,7 +16,8 @@ import java.util.*;
 public class ParamUtil {
 
     private static Document doc;
-    static{
+
+    static {
         SAXReader reader = new SAXReader();
         try {
             doc = reader.read(ParamUtil.class.getClassLoader().getResourceAsStream("parameters.xml"));
@@ -24,80 +25,71 @@ public class ParamUtil {
             e.printStackTrace();
         }
     }
-    public static List<Map<String,String>> genParamMapForFtpAdd(String type,String objName)
+
+    public static List<Map<String, String>> genParamMapForFtpAdd(String type, String objName)
             throws ClassNotFoundException, InvocationTargetException, IllegalAccessException {
-        Element rootElement = doc.getRootElement();
-        List<Element> paramList = rootElement.elements("parameter");
-        List<Map<String,String>> paramMapList = new ArrayList<Map<String, String>>();
-        for(Element parameter:paramList){
-            if(parameter.attributeValue("type").equals(type)){
-                List<Element> objList = parameter.elements("object");
-                for(Element objElement:objList){
-                    if(objElement.attributeValue("name").equals(objName)){
-                        List<Element> objElements = objElement.elements();
-                        Map<String,String> objMap = new LinkedHashMap<String,String>();
-                        for(Element e:objElements){
-                            String elementValue = e.getStringValue();
-                            String id = e.attributeValue("id");
-                            objMap.put(id,elementValue);
-                        }
-                        paramMapList.add(objMap);
-                    }
-                }
+        List<Map<String, String>> paramMapList = new ArrayList<Map<String, String>>();
+        Map<String, String> objMap = new LinkedHashMap<String, String>();
+        List<List<Element>> objElements = getObjElements(type, objName);
+        for (int i = 0; i < objElements.size(); i++) {
+            for (Element e : objElements.get(i)) {
+                String elementValue = e.getStringValue();
+                String id = e.attributeValue("id");
+                objMap.put(id, elementValue);
             }
         }
+        paramMapList.add(objMap);
         return paramMapList;
     }
 
-    public static List<String> genParamForFtpSearch(String type,String objName)
+    public static List<String> genParamForFtpSearch(String type, String objName)
             throws ClassNotFoundException, InvocationTargetException, IllegalAccessException {
-        Element rootElement = doc.getRootElement();
-        List<Element> paramList = rootElement.elements("parameter");
         List<String> paramStrList = new ArrayList<String>();
-        for(Element parameter:paramList){
-            if(parameter.attributeValue("type").equals(type)){
-                List<Element> objList = parameter.elements("object");
-                for(Element objElement:objList){
-                    if(objElement.attributeValue("name").equals(objName)){
-                        List<Element> objElements = objElement.elements();
-                        for(Element e:objElements){
-                            paramStrList.add(e.getText());
-                        }
-                    }
-                }
+        List<List<Element>> objElements = getObjElements(type, objName);
+        for (int i = 0; i < objElements.size(); i++) {
+            for (Element e : objElements.get(i)) {
+                paramStrList.add(e.getText());
             }
         }
         return paramStrList;
     }
 
-    public static List<Map<String,String>> genParamForFtpUpdate(String type,String objName)
+    public static List<Map<String, String>> genParamForFtpUpdate(String type, String objName)
             throws ClassNotFoundException, InvocationTargetException, IllegalAccessException {
+        List<Map<String, String>> paramMapList = new ArrayList<Map<String, String>>();
+        List<List<Element>> objElements = getObjElements(type, objName);
+        for (int i = 0; i < objElements.size(); i++) {
+            for (Element e : objElements.get(i)) {
+                Map<String, String> objMap = new LinkedHashMap<String, String>();
+                String elementValue = e.getStringValue();
+                String id = e.attributeValue("id");
+                objMap.put(id, elementValue);
+                paramMapList.add(objMap);
+            }
+        }
+
+        return paramMapList;
+    }
+
+    public static String getModelClassName(String objName) {
+        String className = objName.substring(0, 1).toUpperCase() + objName.substring(1);
+        return SeleniumAttribute.MODEL_PACKAGE + className;
+    }
+
+    public static List<List<Element>> getObjElements(String type, String objName) {
         Element rootElement = doc.getRootElement();
         List<Element> paramList = rootElement.elements("parameter");
-        List<Map<String,String>> paramMapList = new ArrayList<Map<String, String>>();
-        for(Element parameter:paramList){
-            if(parameter.attributeValue("type").equals(type)){
+        List<List<Element>> objElements = new ArrayList<List<Element>>();
+        for (Element parameter : paramList) {
+            if (parameter.attributeValue("type").equals(type)) {
                 List<Element> objList = parameter.elements("object");
-                for(Element objElement:objList){
-                    if(objElement.attributeValue("name").equals(objName)){
-                        List<Element> objElements = objElement.elements();
-                        for(Element e:objElements){
-                            Map<String,String> objMap = new LinkedHashMap<String,String>();
-                            String elementValue = e.getStringValue();
-                            String id = e.attributeValue("id");
-                            objMap.put(id,elementValue);
-                            paramMapList.add(objMap);
-                        }
-
+                for (Element objElement : objList) {
+                    if (objElement.attributeValue("name").equals(objName)) {
+                        objElements.add(objElement.elements());
                     }
                 }
             }
         }
-        return paramMapList;
-    }
-
-    public static String getModelClassName(String objName){
-        String className = objName.substring(0,1).toUpperCase() + objName.substring(1);
-        return SeleniumAttribute.MODEL_PACKAGE + className;
+        return objElements;
     }
 }
